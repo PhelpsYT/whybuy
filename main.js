f31bf352cb8f933731bd728290cab22eb401512c
Filipe Catarino <phelpsyt@gmail.com>
2025-05-21 17:08:02 +0100

    First Layout

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log("WhyBuy script loaded!");
+console.log("WhyBuy loaded");
+
+document.getElementById("searchBox").addEventListener("input", function (e) {
+  console.log("User is searching for:", e.target.value);
+});
